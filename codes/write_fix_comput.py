6bb7955885dffbedf22d49bc2566d74cafe6052a
Saeed Amiri <samiri@uni-bremen.de>
2022-08-26 14:57:40 +0200

    PEP08

--- codes/write_fix_comput.py
+++ codes/write_fix_comput.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from colors_text import TextColor as bcolors
 
+
 class Doc:
     """write LAMMPS commands in a file name as: `setting.lmp`
     It will contains:
@@ -14,7 +15,7 @@ class Doc:
         include copmute and fix for all the pairs in the final datafile
     Density profile:
         for each  group (i.e. each file)
-    
+
     Input:
         A DataFrame contains inforamtion about atoms type and name
     Output:
