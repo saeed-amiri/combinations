f4a2d8cf0b58ad64e44ee65b2d08f56da5e6a0e2
Saeed Amiri <samiri@uni-bremen.de>
2022-08-26 15:36:49 +0200

    initiate

--- codes/write_fix_comput.py
+++ codes/write_fix_comput.py
@@ -8,6 +8,7 @@ from colors_text import TextColor as bcolors
 
 class Doc:
     """write LAMMPS commands in a file name as: `setting.lmp`
+    if it asked in the struct file
     It will contains:
     Group:
         number of group will be defeind by the number of files
@@ -21,3 +22,27 @@ class Doc:
     Output:
         A file named: `setting.lmp`
     """
+
+class WriteGroup:
+    """write groups for each file"""
+    def __init__(self) -> None:
+        print(f'{self.__class__.__name__}')
+
+
+class WriteDistribution:
+    """write commands for calculating ditribution function for all
+    the pairs"""
+    def __init__(self) -> None:
+        print(f'{self.__class__.__name__}')
+
+
+class WriteFix(WriteGroup, # Write group information
+               WriteDistribution  # Write radial distribution function
+               ):
+    """call all the classes and write the commands"""
+    def __init__(self) -> None:
+        fname: str = 'setting.lmp'  # Name of the output file
+        print(f'{bcolors.OKCYAN}\n{self.__class__.__name__}:\n'
+              f'\tWriting: `{fname}`{bcolors.ENDC}\n')
+        WriteGroup.__init__(self)
+        WriteDistribution.__init__(self)
