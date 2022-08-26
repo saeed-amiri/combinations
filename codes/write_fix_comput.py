8bc152812c336433fa7391946cda3d43c73707a0
Saeed Amiri <samiri@uni-bremen.de>
2022-08-26 14:52:02 +0200

    PEP08

---
+++ codes/write_fix_comput.py
@@ -0,0 +1,22 @@
+import re
+import typing
+import itertools
+import numpy as np
+import pandas as pd
+from colors_text import TextColor as bcolors
+
+class Doc:
+    """write LAMMPS commands in a file name as: `setting.lmp`
+    It will contains:
+    Group:
+        number of group will be defeind by the number of files
+    Radial distutbution function:
+        include copmute and fix for all the pairs in the final datafile
+    Density profile:
+        for each  group (i.e. each file)
+    
+    Input:
+        A DataFrame contains inforamtion about atoms type and name
+    Output:
+        A file named: `setting.lmp`
+    """
--- codes/write_param.py
+++ codes/write_param.py
@@ -1,4 +1,3 @@
-from asyncore import write
 import re
 import typing
 import itertools
