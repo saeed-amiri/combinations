7969ce71a3abb4bea18778392ac606ce5ff4f572
Saeed Amiri <samiri@uni-bremen.de>
2022-10-24 14:41:16 +0200

    rotate the data

---
+++ codes/rotate_data.py
@@ -0,0 +1,17 @@
+import sys
+import pandas as pd
+import read_lmp_data as relmp
+
+class Doc:
+    """rotate the pd datframe of the data around the center of mass
+    of the data.
+    It will rotate about x axis and counterclocwise
+    input:
+        Data in the DataFrame
+        Angle of rotation
+    output:
+        DataFrame
+    """
+
+if __name__ == "__main__":
+    "rotate the data"
